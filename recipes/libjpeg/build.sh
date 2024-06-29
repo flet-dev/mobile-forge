2f4db70e64a022357ef517133365f5e9b1db5a01
Feodor Fitsner <feodor@appveyor.com>
2024-06-28 19:02:08 -0700

    Fix libjpeg and pillow, fail entire build on error

--- recipes/libjpeg/build.sh
+++ recipes/libjpeg/build.sh
@@ -5,6 +5,7 @@ set -eu
 if [ $CROSS_VENV_SDK == "android" ]; then
     cmake -G"Unix Makefiles" \
         -DCMAKE_SYSTEM_NAME=Android \
+        -DANDROID_PLATFORM=24 \
         -DANDROID_ABI=$ANDROID_ABI \
         -DCMAKE_TOOLCHAIN_FILE=$NDK_ROOT/build/cmake/android.toolchain.cmake \
         -DWITH_SIMD=OFF \
