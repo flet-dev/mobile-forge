448b79db838a364f19a010e011ab684477839964
Feodor Fitsner <feodor@appveyor.com>
2024-10-21 17:26:38 -0700

    flet-libjpeg recipe

---
+++ recipes/flet-libjpeg/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eu
+
+# SIMD is only available for x86, so disable for consistency between ABIs.
+if [ $CROSS_VENV_SDK == "android" ]; then
+    cmake -G"Unix Makefiles" \
+        -DCMAKE_SYSTEM_NAME=Android \
+        -DANDROID_PLATFORM=24 \
+        -DANDROID_ABI=$ANDROID_ABI \
+        -DCMAKE_TOOLCHAIN_FILE=$NDK_ROOT/build/cmake/android.toolchain.cmake \
+        -DWITH_SIMD=OFF \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX .
+else
+    cmake -G"Unix Makefiles" \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX .
+fi
+
+make -j $CPU_COUNT > /dev/null 2>&1 || { echo "Error building libjpeg"; exit 1; }
+make install
+
+rm -r $PREFIX/{bin,share}
+rm -r $PREFIX/lib/{pkgconfig,cmake}
