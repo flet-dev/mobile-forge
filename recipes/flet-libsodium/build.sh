b1b9ec67f1dbbd120b685cf805786b766289cf2c
Feodor Fitsner <feodor@appveyor.com>
2025-01-01 13:03:19 -0800

    flet-libsodium for Android and iOS

--- recipes/flet-libsodium/build.sh
+++ recipes/flet-libsodium/build.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 set -eu
 
-./configure --host=$HOST_TRIPLET --prefix=$PREFIX
+if [ $CROSS_VENV_SDK != "android" ]; then
+    case $HOST_TRIPLET in
+        arm64-apple-ios)
+            HOST_TRIPLET=arm-apple-darwin23
+            ;;
+        arm64-apple-ios-simulator)
+            HOST_TRIPLET=aarch64-apple-darwin23
+            ;;
+        x86_64-apple-ios-simulator)
+            HOST_TRIPLET=x86_64-apple-darwin23
+            ;;
+        *)
+            echo "Unknown host triplet: '$HOST_TRIPLET'"
+            exit 1
+            ;;
+    esac
+fi
+
+./configure --host=$HOST_TRIPLET --prefix=$PREFIX --disable-soname-versions
 make -j $CPU_COUNT
 make install
 
-rm -r $PREFIX/lib/{*.la,pkgconfig}
+rm -r $PREFIX/lib/{*.a,*.la,pkgconfig}
+
+if [ $CROSS_VENV_SDK != "android" ]; then
+    mv $PREFIX/lib/libsodium.dylib $PREFIX/lib/libsodium.so
+fi
